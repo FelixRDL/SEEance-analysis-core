d52e8894f0c45d5fc356fa57c27c729c9c4b5523
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-06-21 15:49:35 +0200

    Implement analysis

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ var git = require("simple-git").gitP;
 const pathLib = require("path");
 const fs = require("fs");
 
-module.exports = async function(repoOwner, repoName, preprocessors, analyses, token=undefined) {
-    const rp = await ComponentProvider(token);
-    const datasourceNames = ['issues', 'milestones', 'git-authors'];
+/**
+ *
+ * @param repoOwner
+ * @param repoName
+ * @param preprocessors
+ * @param {{
+ *     pkg: Object,
+ *     module: function
+ *     config: Object
+ * }} analysis
+ * @param token
+ * @returns {Promise<void>}
+ */
+module.exports = async function (repoOwner, repoName, preprocessors, analysis, token = undefined) {
+    const rp = await ComponentProvider({
+        token: token,
+        customRepositories: ['felixrdl/seeance-test']
+    });
+    const datasourceNames = analysis.pkg.seeance.depends_on;
 
     // TODO: implement installing dependencies
     await rp.init();
@@ -15,27 +31,42 @@ module.exports = async function(repoOwner, repoName, preprocessors, analyses, to
     var datasources = datasourceNames.map(ds => rp.getDatasourceByName(ds));
 
     const githubDatasources = datasources.filter((ds) => ds['manifest']['type'].includes('github'));
-    const gitDatasources = datasources.filter((ds) => !ds['manifest']['type'].includes('git'));
-
-    const input = await Promise.all(
-        githubDatasources.map((ds) => ds.module(repoOwner, repoName, token)),
-        getRepository(`https://github.com/${repoOwner}/${repoName}`).then((path) => {
-            return gitDatasources.map((ds) => ds.module(path, token));
-        })
+    const gitDatasources = datasources.filter((ds) => ds['manifest']['type'].endsWith('git'));
+    const repoPath = await getRepository(`https://github.com/${repoOwner}/${repoName}`);
+    let input = await Promise.all(
+        gitDatasources.map(async (ds) => {
+            return {
+                result: await ds.module(repoPath, token),
+                manifest: ds.manifest,
+                package: ds.package,
+            }
+        }).concat(
+            githubDatasources.map(async (ds) => {
+                return {
+                    result: await ds.module(repoOwner, repoName, token),
+                    manifest: ds.manifest,
+                    package: ds.package
+                }
+            }))
     );
+    input = input.reduce(function(acc, curr) {
+        acc[curr.package.name] = curr.result;
+        return acc;
+    }, {});
 
     // TODO: preprocessing
 
-    // TODO: execute analysis
+    // TODO: pass in real visualisation object
+    return await analysis.module(input, analysis.config, {})
 }
 
 
-async function getRepository(path, token=undefined) {
+async function getRepository(path, token = undefined) {
     const localPath = pathLib.join(__dirname, '.repos');
-    if(!fs.existsSync(localPath))
+    if (!fs.existsSync(localPath))
         fs.mkdirSync(localPath);
     const target = pathLib.join(localPath, pathLib.basename(path))
-    if(fs.existsSync(target)) {
+    if (fs.existsSync(target)) {
         await git(target).pull();
     } else {
         await git().clone(path, target);
--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 const core = require('./index');
 
-core(process.argv[2], process.argv[3]).then(result => {
+core(process.argv[2], process.argv[3], [], {
+    config: {},
+    pkg: {
+        seeance: {
+            depends_on: ['issues', 'milestones', 'git-authors']
+        }
+    },
+    module: async (input, config, visualisation) => {
+        return "<h1>Sampleee</h1>"
+    }
+}).then(result => {
     console.log(result);
 }, error => {
     console.error(error);
