1346809ade8fb1ae82bec6762cb733eca10e62a2
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-06-21 12:13:14 +0200

    Implement datasource handling

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const ComponentProvider = require("./lib/repository-provider")
+const ComponentProvider = require("./lib/repository-provider");
+var git = require("simple-git").gitP;
+const pathLib = require("path");
+const fs = require("fs");
 
 module.exports = async function(repoOwner, repoName, preprocessors, analyses, token=undefined) {
     const rp = await ComponentProvider(token);
+    const datasourceNames = ['issues', 'milestones', 'git-authors'];
+
+    // TODO: implement installing dependencies
     await rp.init();
 
-    const issues = await rp.getDatasourceByName('issues')(repoOwner, repoName);
-    const milestones = await rp.getDatasourceByName('milestones')(repoOwner, repoName);
-    console.log(issues);
-    console.log(milestones);
+    // Acquire input data
+    // TODO: implement caching
+    var datasources = datasourceNames.map(ds => rp.getDatasourceByName(ds));
+
+    const githubDatasources = datasources.filter((ds) => ds['manifest']['type'].includes('github'));
+    const gitDatasources = datasources.filter((ds) => !ds['manifest']['type'].includes('git'));
+
+    const input = await Promise.all(
+        githubDatasources.map((ds) => ds.module(repoOwner, repoName, token)),
+        getRepository(`https://github.com/${repoOwner}/${repoName}`).then((path) => {
+            return gitDatasources.map((ds) => ds.module(path, token));
+        })
+    );
+
+    // TODO: preprocessing
+
+    // TODO: execute analysis
+}
+
+
+async function getRepository(path, token=undefined) {
+    const localPath = pathLib.join(__dirname, '.repos');
+    if(!fs.existsSync(localPath))
+        fs.mkdirSync(localPath);
+    const target = pathLib.join(localPath, pathLib.basename(path))
+    if(fs.existsSync(target)) {
+        await git(target).pull();
+    } else {
+        await git().clone(path, target);
+    }
+    return target;
 }
